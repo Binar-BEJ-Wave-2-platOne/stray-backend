77f653226578958b6104900ae15ca698b09da222
tamir3232 <tamirrasydi@gmail.com>
2022-08-24 21:36:41 +0700

    menambhakan delete dan update

--- src/controllers/orders.controllers.js
+++ src/controllers/orders.controllers.js
@@ -1,4 +1,5 @@
 const { verify } = require('jsonwebtoken')
+const { where } = require('sequelize/types')
 const { Orders, Users, Promos } = require('../models/index')
 
 const CreateOrder = async(req, res, next) => {
@@ -18,7 +19,7 @@ const CreateOrder = async(req, res, next) => {
             }
         }
 
-        const promoExist = Promos.findOne({
+        const promoExist = await Promos.findOne({
             where: {
                 id: bodies.id_promo,
             }
@@ -45,6 +46,63 @@ const CreateOrder = async(req, res, next) => {
 
 
 
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+const UpdateOrder = (req, res, next) => {
+
+    try {
+        const bodies = req.body
+        const orderExist = await Orders.update({
+            where: {
+                id: Number(req.params.id)
+            }
+
+        })
+        if (!orderExist) {
+            throw {
+                code: 404,
+                message: "BARANG TIDAK TERSEDIA"
+            }
+        }
+
+        const order = await Orders.update({
+            sender_addres: bodies.sender_addres,
+            receiver_address: bodies.receiver_address,
+            total_price: bodies.total_price,
+        }, {
+            where: {
+                id: orderExist.id
+            }
+        })
+
+        return res.status(201).json({
+            code: 201,
+            message: "PESAN SUDAH TERUPDATE"
+        })
+    } catch (error) {
+        next(error)
+    }
+
+
+}
+
+
+const deleteOrder = (res, req, next) => {
+    const bodies = req.body
+    try {
+        Orders.destroy({
+            where: {
+                id: Number(req.params.id)
+            }
+        })
+        return res.status(200).json({
+            code: 200,
+            message: "ORDER TELAH DI BATALKAN"
+        })
     } catch (error) {
         next(error)
     }
