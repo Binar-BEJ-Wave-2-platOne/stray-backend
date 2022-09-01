5eab18d0419e5b76edc5ae2427313c6386768601
SatyaWJY <standhawijaya@gmail.com>
2022-09-01 20:12:29 +0700

    add endpoint promos

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,6 +4,7 @@ const app = express();
 const hello = require('../routes/hello.routes');
 const items = require('../routes/items.routes');
 const auth = require('../routes/auth.routes');
+const promos = require('../routes/promos.routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,7 @@ app.group('/api/v1', (router) => {
   router.use('/', hello);
   router.use('/items', items);
   router.use('/auth', auth);
+  router.use('/promos', promos)
 });
 
 module.exports = app;
