b6fd7fd83748bcdfbfcbb588f67982099cf81708
SatyaWJY <standhawijaya@gmail.com>
2022-09-08 20:14:55 +0700

    Add update and delete cart

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,1 +1,51 @@
 const { sequelize, Items } = require('../models')
+
+const updateCart = async (req, res, next) => {
+    try {
+        const id = req.params.id
+
+        const findCart = await Cart.findByPk(id)
+        if (!findCart) {
+            throw {
+                code: 404,
+                message: 'Cart not found',
+            }
+        }
+
+        await findCart.update(req.body)
+
+        return res.status(200).json({
+            message: 'Update cart success',
+            data: findCart,
+        })
+    } catch (error) {
+        next(error)
+    } 
+}
+
+const deleteCart = async (res, req, next) => {
+    try {
+        const id = req.params.id
+        const findCart = await Cart.findByPk(id)
+
+        if (!findCart) {
+            throw {
+                code: 404, 
+                message: 'Cart not found',
+            }
+        }
+
+        await findCart.destroy()
+
+        return res.status(200).json({
+            message: 'Delete cart success',
+        })
+    } catch (error) {
+       next(error) 
+    }
+}
+
+module.exports = {
+    updateCart,
+    deleteCart
+}
