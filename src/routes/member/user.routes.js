35a104a2c9890207bffc711eac58c139065d41ac
Akhmad Maknur <akhmad.maknur@gmail.com>
2022-09-12 15:10:14 +0800

    feat: add swagger user

--- src/routes/member/user.routes.js
+++ src/routes/member/user.routes.js
@@ -3,7 +3,15 @@ const router = require('express').Router()
 const { getOneUser, updateUser } = require('../../controllers/user.controller')
 const { isTokenValid } = require('../../middlewares/verifyToken.middleware')
 
+const validation = require('../../middlewares/validation.middleware')
+const { userSchemaMember } = require('../../schemas/updateuser.schema')
+
 router.get('/', isTokenValid('MEMBER'), getOneUser)
-router.patch('/', isTokenValid('MEMBER'), updateUser)
+router.patch(
+    '/',
+    isTokenValid('MEMBER'),
+    validation(userSchemaMember),
+    updateUser
+)
 
 module.exports = router
