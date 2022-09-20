0d2671f61478b0229f37b8665b65516ad2a7792d
tamir3232 <tamirrasydi@gmail.com>
2022-09-20 21:03:37 +0700

    menambah web socket

--- index.js
+++ index.js
@@ -10,8 +10,23 @@ const swaggerJson = require('./openapi.json')
 const swaggerUi = require('swagger-ui-express')
 
 const express = require('express')
+
 const app = express()
 const port = process.env.PORT || 3000
+    // const server = require('./server')
+
+// const io = socketio(server);
+
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+//     socket.on('chat message', (msg) => {
+//         console.log('message : ' + msg);
+//     })
+//     socket.on('disconnect', () => {
+//         console.log('user disconected');
+//     })
+
+// })
 
 app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerJson))
 app.use(express.json())
