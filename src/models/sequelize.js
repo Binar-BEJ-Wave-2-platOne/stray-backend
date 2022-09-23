dede224d794bccbddcafb6c5458dbe3b1ad4a4c5
Akhmad Maknur <akhmad.maknur@gmail.com>
2022-09-23 15:16:23 +0800

    Update sequelize.js

--- src/models/sequelize.js
+++ src/models/sequelize.js
@@ -2,9 +2,10 @@ const { config } = require('dotenv')
 const Sequelize = require('sequelize')
 const configs = require('../../database/config/config')
 
+const sequelize = new Sequelize(configs.database, configs.username, configs.password, { // database , username , password
+    dialect: configs.dialect,
+    host: configs.host,
+    port: parseInt(configs.port)
+})
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
 module.exports = sequelize
